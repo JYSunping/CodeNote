feeea03585bbe2cad1a58cf8acdd497934c9fe69
Erona <erona@loli.bz>
2015-12-11 15:52:55 +0800

    init Similary.java

---
+++ src/Similary.java
@@ -0,0 +1,11 @@
+class Similary
+{
+    public static String delstr(String str)
+    {
+        return str;
+    }
+    public static double similary(String c1,String c2)
+    {
+        return (double)c1.length()/c2.length();
+    }
+}
