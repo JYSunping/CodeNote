27613106e422b1b46b97bcdb3bda6f78af049a56
Erona <erona@loli.bz>
2015-12-13 17:32:15 +0800

    add DB.java

---
+++ src/DB.java
@@ -0,0 +1,23 @@
+class DB
+{
+    public static boolean add(String name)
+    {
+        return true;
+    }
+    public static void delete(String name)
+    {
+
+    }
+    public static void edit(String name,String code)
+    {
+
+    }
+    public static String getCode(String name)
+    {
+        return "int main";
+    }
+    public static String[] getList()
+    {
+        return new String[]{"0-1backpack","SPFA","Dijkstra","ISAP","Dinic","MaxMatch"};
+    }
+}
